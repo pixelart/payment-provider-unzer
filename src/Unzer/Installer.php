7d20a72845c030bbc8725cf40e37e93a9ef47464
Marco Lumia <m.lumia@pixelart.at>
2023-11-20 11:02:34 +0100

    Update type declaration in Installer

--- src/Unzer/Installer.php
+++ src/Unzer/Installer.php
@@ -19,9 +19,9 @@ use Pimcore\Bundle\EcommerceFrameworkBundle\Tools\PaymentProviderInstaller;
 
 class Installer extends PaymentProviderInstaller
 {
-    protected $bricksPath = __DIR__ . '/../../install/objectbrick_sources/';
+    protected string $bricksPath = __DIR__ . '/../../install/objectbrick_sources/';
 
-    protected $bricksToInstall = [
+    protected array $bricksToInstall = [
         'PaymentProviderUnzer' => 'objectbrick_PaymentProviderUnzer_export.json'
     ];
 }
